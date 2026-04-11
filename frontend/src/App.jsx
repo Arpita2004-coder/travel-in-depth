dccbde182895d83f777156bde692ea09eadf3e30
Arpit1825 <2k24.csaiml1a2413757@gmail.com>
2026-04-11 16:07:04 +0530

    Added footer,features and stats bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import Navbar from './components/layout/Navbar';
 import AppRouter from './routes/AppRouter';
-
+import Footer from './components/layout/Footer';
+import Featured from './components/layout/Featured';
+import Stats from './components/stats/Stats';
 export default function App() {
   return (
     <div className="min-h-screen bg-color: #FAFAFA;">
       <Navbar />
-      
-      <AppRouter /> 
-      
-      <div className="h-[200vh]"></div>
+      <AppRouter />
+      <Featured />
+      <Stats/>
+       <Footer />
     </div>
   );
 }
